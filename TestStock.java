92b9091a0038662d69e4668bc8f6100446a34d0b
Sanaullah Ulfat <ulfatsanaullah45@gmail.com>
2025-12-23 23:09:52 +0500

    Update TestStock.java

---
+++ TestStock.java
@@ -0,0 +1,9 @@
+public class TestStock {
+    public static void main(String[] args) {
+        Stock stock = new Stock("ORCL", "Oracle Corporation");
+        stock.previousClosingPrice = 34.5;
+        stock.currentPrice = 34.35;
+
+        System.out.println("Price Change Percentage: " + stock.getChangePercent() + "%");
+    }
+}
