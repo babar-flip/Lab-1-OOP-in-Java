ed378c3d1e9bd2fc08d74f61b6de7f2684e4104f
Sanaullah Ulfat <ulfatsanaullah45@gmail.com>
2025-12-23 23:09:36 +0500

    Update Stock.java

---
+++ Stock.java
@@ -0,0 +1,15 @@
+public class Stock {
+    String symbol;
+    String name;
+    double previousClosingPrice;
+    double currentPrice;
+
+    Stock(String s, String n) {
+        symbol = s;
+        name = n;
+    }
+
+    double getChangePercent() {
+        return ((currentPrice - previousClosingPrice) / previousClosingPrice) * 100;
+    }
+}
