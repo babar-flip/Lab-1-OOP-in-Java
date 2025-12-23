92c8df216497b12669a29ea5d0c4b3b2f3334136
Sanaullah Ulfat <ulfatsanaullah45@gmail.com>
2025-12-23 23:09:17 +0500

    Update TestRectangle.java

---
+++ TestRectangle.java
@@ -0,0 +1,14 @@
+public class TestRectangle {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(4, 40);
+        Rectangle r2 = new Rectangle(3.5, 35.9);
+
+        System.out.println("Rectangle 1");
+        System.out.println("Area: " + r1.getArea());
+        System.out.println("Perimeter: " + r1.getPerimeter());
+
+        System.out.println("\nRectangle 2");
+        System.out.println("Area: " + r2.getArea());
+        System.out.println("Perimeter: " + r2.getPerimeter());
+    }
+}
